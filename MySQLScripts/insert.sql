8f60e5197d4f713e1fadc8fd6d76290bfb76b599
viktor <victores77@gmail.com>
2018-11-06 18:15:39 +0200

    Initial commit

---
+++ MySQLScripts/insert.sql
@@ -0,0 +1,67 @@
+USE transportation;
+
+INSERT INTO transits
+VALUES (1, 'Apples', 2000, '2018/07/3');
+INSERT INTO transits
+VALUES (2, 'Grape', 1050, '2018/07/5');
+INSERT INTO transits
+VALUES (3, 'Pears', 1300, '2018/07/7');
+INSERT INTO transits
+VALUES (4, 'Chocolate', 800, '2018/07/8');
+INSERT INTO transits
+VALUES (5, 'Melon', 3000, '2018/07/9');
+INSERT INTO transits
+VALUES (6, 'Peach', 1000, '2018/07/21');
+INSERT INTO transits
+VALUES (7, 'Watermelon', 10000, '2018/08/12');
+INSERT INTO transits
+VALUES (8, 'Sweets', 800, '2018/08/15');
+INSERT INTO transits
+VALUES (9, 'Beet', 4000, '2018/08/17');
+INSERT INTO transits
+VALUES (10, 'Potato', 10000, '2018/08/20');
+
+INSERT INTO transport
+VALUES (1, 'MAN', 1, 1);
+INSERT INTO transport
+VALUES (2, 'DAFF', 1, 1);
+INSERT INTO transport
+VALUES (3, 'MAN', 1, 2);
+INSERT INTO transport
+VALUES (4, 'MAN', 3, 3);
+INSERT INTO transport
+VALUES (5, 'Mercedes', 2, 2);
+INSERT INTO transport
+VALUES (6, 'DAFF', 3, 4);
+INSERT INTO transport
+VALUES (7, 'MAN', 2, 5);
+INSERT INTO transport
+VALUES (8, 'Mercedes', 4, 5);
+INSERT INTO transport
+VALUES (9, 'DAFF', 4, 6);
+INSERT INTO transport
+VALUES (10, 'MAN', 2, 7);
+INSERT INTO transport
+VALUES (11, 'Vovlvo', 1, 7);
+INSERT INTO transport
+VALUES (12, 'Volvo', 4, 7);
+INSERT INTO transport
+VALUES (13, 'DAFF', 4, 8);
+INSERT INTO transport
+VALUES (14, 'MAN', 3, 8);
+INSERT INTO transport
+VALUES (15, 'DAFF', 3, 9);
+INSERT INTO transport
+VALUES (16, 'MAN', 2, 10);
+INSERT INTO transport
+VALUES (16, 'MAN', 2, 10);
+
+
+INSERT INTO company
+VALUES (1, 'Forex', 120);
+INSERT INTO company
+VALUES (2, 'Fox', 80);
+INSERT INTO company
+VALUES (3, 'Delivery', 210);
+INSERT INTO company
+VALUES (4, 'Novanym', 190);
